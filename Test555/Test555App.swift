d64fee8c3105b9dd888f1d710550daee8326044e
BigJermaine <113020989+bigjermaine@users.noreply.github.com>
2024-12-27 06:53:30 +0100

    Initial Commit

---
+++ Test555/Test555App.swift
@@ -0,0 +1,17 @@
+//
+//  Test555App.swift
+//  Test555
+//
+//  Created by MacBook AIR on 27/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct Test555App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
